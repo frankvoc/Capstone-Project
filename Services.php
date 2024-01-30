13a591a5d4c99b47a707abac7407343d97551e60
wingo65 <122292864+wingo65@users.noreply.github.com>
2024-01-30 12:30:34 -0500

    added files

---
+++ Services.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le Couturier</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Island+Moments&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Jacques+Francois&display=swap" rel="stylesheet">
+    <style>
+       .island-moments {
+        font-family: 'Island Moments', sans-serif;
+      }
+      .italiana{
+        font-family: 'Italiana', sans-serif;
+      }
+      .jacques{
+        font-family: 'Jacques Francois', serif;
+      }
+      .larger-text{
+        font-size:2.5rem;
+      }
+     
+    
+    </style>
+</head>
+
+<body style="background-color: #F5EAEB;">
+    <!--Top bar-->
+    <div class="py-3 px-5 flex justify-between items-center" style="background-color:#C1373C ;">
+      <div class="flex items-center">
+        <div class="bg-white border border-black py-2 px-4 rounded">
+        <a href="#" class="text-xl font-bold" style="color: #152266;">Le Couturier</a>
+      </div>
+      </div>
+      <div class="flex items-center gap-4">
+        <div class="bg-white border border-black py-2 px-4 rounded">
+        <a href="#" class="text-lg font-bold island-moments"style="color: #152266;">Sign In</a>
+      </div>
+      </div>
+    </div>
+  <!--Big image with text-->
+  <div class="relative">
+    <img src="img/how-to-find-a-tailor.jpg" class="w-full h-96 object-cover">
+    <div class="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-25 flex items-center justify-center">
+      <button class="text-white text-6xl font-bold jacques" href="#">Book by Service</button>
+    </div>
+  </div>
+
+  <div class="relative h-32 w-64 ...">
+    <div class="absolute left-0 top-0 h-16 w-16 ...">
+      <div class="size-40 ...">
+        <img src="img/shutterstock_655460896.webp" alt="Avatar" style="border-radius: 50%;" >
+      </div>
+    </div>
+  </div>
+
+  <!-- Span left edge -->
+<div class="relative h-32 w-32 ...">
+  <div class="absolute top-28 left-0 w-16 ...">
+    <div class="size-40 ...">
+      <img src="img/shutterstock_655460896.webp" alt="Avatar" style="border-radius: 50%;">
+    </div>
+  </div>
+</div>
+
+  <div class="py-10" style="background-color: #F5EAEB;">
+    <div class="container mx-auto grid grid-cols-1 md:grid-cols-2">
+      <div style="color: #99382C;">
+        <h3 class="text-2xl font-bold mb-3">1738 Massachusetts Ave Cambridge, MA 02138</h3>
+        <p>Get Directions</p>
+        <p class="my-3">(617) 497-1258</p>
+        <p class="text-sm">Mon: 9:00AM - 6:00PM</p>
+        <p class="text-sm">Mon: 9:00AM - 6:00PM</p>
+        <p class="text-sm">Mon: 9:00AM - 6:00PM</p>
+        <p class="text-sm">Mon: 9:00AM - 6:00PM</p>
+        <p class="text-sm">Mon: 9:00AM - 6:00PM</p>
+      </div>
+      <!--Embeded Google Map-->
+      <div class="google-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2947.0245575995805!2d-71.12219652266681!3d42.3846233335475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e3775408348aa1%3A0x515c1bbec9ee6f9!2sLe%20Couturier%20House%20of%20Alterations!5e0!3m2!1sen!2sus!4v1706548730171!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+  </div>
