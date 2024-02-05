d58d5fc821603a31410ced9695860cde58f3cc7e
wingo65 <122292864+wingo65@users.noreply.github.com>
2024-02-05 13:58:31 -0500

    updated contact.php
    changed a couple things in in the contact.php file

--- contact.php
+++ contact.php
@@ -1,25 +1,24 @@
 <?php
-// Get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
- 
-$to = "wingo6590@gmail.com";
-$subject = "This is the subject line";
- 
-// The following text will be sent
-// Name = user entered name
-// Email = user entered email
-// Message = user entered message 
-$txt ="Name = ". $name . "\r\n  Email = "
-    . $email . "\r\n Message =" . $message;
- 
-$headers = "From: noreply@demosite.com" . "\r\n" .
-            "CC: somebodyelse@example.com";
-if($email != NULL) {
-    mail($to, $subject, $txt, $headers);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get data from form  
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $message = isset($_POST['message']) ? $_POST['message'] : '';
+    
+    $to = "wingo6590@gmail.com";
+    $subject = "This is the subject line";
+    
+    // The following text will be sent
+    $txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Message = " . $message;
+    
+    $headers = "From: noreply@demosite.com" . "\r\n" .
+               "CC: somebodyelse@example.com";
+
+    if($email) {
+        mail($to, $subject, $txt, $headers);
+        // Redirect to thank you page
+        header("Location: last.html");
+        exit;
+    }
 }
- 
-// Redirect to
-header("Location:last.html");
 ?>
