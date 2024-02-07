10f401307cbd1c46bc7f5ed4109bdafb67e5741b
frankvoc <fmvocatura@email.neit.edu>
2024-02-07 18:47:06 -0500

    Update adminDashboard.php
    Fixed file pathing

--- backend/adminDashboard.php
+++ backend/adminDashboard.php
@@ -1,3 +1,3 @@
 <div class="bg-white border border-black py-2 px-4 rounded">
-      <a href="adminLogin.php" class="text-xl font-bold" style="color: #152266;">Le Couturier</a>
+      <a href="../adminLogin.php" class="text-xl font-bold" style="color: #152266;">Le Couturier</a>
     </div>
