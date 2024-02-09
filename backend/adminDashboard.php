73cf40f30d914854c5116f7b0e88a0e36df441e6
wingo65 <122292864+wingo65@users.noreply.github.com>
2024-02-09 14:02:03 -0500

    Added php and db files
    After getting the contact us form functional I added the login files and admin database files, I also added a model_clients.php file so I can get the dashboard ready to be funtional

---
+++ backend/adminDashboard.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Dashboard</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Island+Moments&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Jacques+Francois&display=swap" rel="stylesheet">
+  <style>
+     .island-moments {
+      font-family: 'Island Moments', sans-serif;
+    }
+    .italiana{
+      font-family: 'Italiana', sans-serif;
+    }
+    .jacques{
+      font-family: 'Jacques Francois', serif;
+    }
+    .larger-text{
+      font-size:2.5rem;
+    }
+    .dm-serif{
+        font-family: 'DM Serif Display', serif; 
+    }
+  </style>
+</head>
+<body style="background-color: #F5EAEB;">
+    <div class="min-h-screen flex flex-col items-center justify-center">
+        <header class="absolute top-0 w-full bg-red-600 text-white text-center py-4 text-lg font-semibold jacques"style="background-color:#C1373C ;">
+            Admin Login
+        </header>
+    <div class="flex min-h-screen items-center justify-center">
+    <div class="flex-1 flex justify-center">
+    <div class="bg-white border border-black py-2 px-4 rounded">
+        <a href="../adminLogin.php" class="text-xl font-bold" style="color: #152266;">Le Couturier</a>
+    </div>
+</body>
+</html>
